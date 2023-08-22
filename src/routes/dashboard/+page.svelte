bdcfea45852c9249feae0df9e7686ecab343e9de
kwandapchumba <kwandapchumba@gmail.com>
2023-08-22 11:16:39 +0300

    ui improvements

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -788,8 +788,12 @@
 					word-break: break-word;
 					flex-grow: 1;
 
-					@media only screen and (max-width: 768px) {
+					@media only screen and (max-width: 568px) {
 						max-width: 50rem;
+					}
+
+					@media only screen and (min-width: 728px) and (max-width: 425px) {
+						max-width: 30rem;
 					}
 
 					.thumbnail {
