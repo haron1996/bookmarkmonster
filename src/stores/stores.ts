a3b219efba702682a21f90e9acaa4468ce338c0a
kwandapchumba <kwandapchumba@gmail.com>
2023-09-13 18:17:08 +0300

    build error fixes

--- src/stores/stores.ts
+++ src/stores/stores.ts
@@ -4,9 +4,9 @@ import type { Bookmark } from '../types/bookmark';
 import type { Session } from '../types/session';
 import type { Folder } from '../types/folder';
 
-//export const apiHost = readable<string>('https://api.bookmarkmonster.xyz');
+export const apiHost = readable<string>('https://api.bookmarkmonster.xyz');
 
-export const apiHost = readable<string>('http://localhost:5000');
+//export const apiHost = readable<string>('http://localhost:5000');
 
 export const sideBarWidth = writable<number>(25);
 
